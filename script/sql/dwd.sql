108e395444b47c053a123508fe62b9b19bdc32e9
唐修红 <tangxiuhong@qgs-china.com>
2024-09-01 11:12:23 +0800

    优化关键词

--- script/sql/dwd.sql
+++ script/sql/dwd.sql
@@ -1,16 +1,15 @@
-create database dwd;
-use dwd;
-DROP TABLE IF EXISTS ods_aweme_comment;
+DROP TABLE IF EXISTS dwd_aweme_comment;
 CREATE TABLE IF NOT EXISTS ods_aweme_comment (
     id int NOT NULL AUTO_INCREMENT COMMENT '自增ID',
     platform varchar(10) NOT NULL COMMENT '平台ID',
     aweme_id varchar(64) NOT NULL COMMENT '视频ID',
-    user_id varchar(256) DEFAULT NULL COMMENT '用户id',
     comment_id varchar(256) DEFAULT NULL COMMENT '评论id',
+    user_id varchar(256) DEFAULT NULL COMMENT '用户id',
+    user_url varchar(256) DEFAULT NULL COMMENT '用户url',
+    aweme_url  varchar(100) DEFAULT NULL comment '视频链接',
     nickname varchar(64) DEFAULT NULL COMMENT '昵称',
     ip_location varchar(10) DEFAULT NULL COMMENT '用户ip地址',
     content longtext DEFAULT NULL COMMENT '评论内容',
-    user_url varchar(256) DEFAULT NULL COMMENT '用户url',
     create_time datetime COMMENT '评论时间',
     keyword  varchar(64) DEFAULT NULL COMMENT '关键词',
     PRIMARY KEY (id),
@@ -34,3 +33,16 @@ create table IF NOT EXISTS  dwd_crawler_aweme_task
     UNIQUE `dact_un_idx1` USING BTREE (`platform`,`aweme_id`)
     ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '评论爬虫任务信息表' ROW_FORMAT = Dynamic;
 
+
+
+
+    aweme_id,
+    comment_id,
+    user_id,
+    user_url,
+    aweme_url,
+    nickname,
+    ip_location,
+    content,
+    create_time ,
+    keyword,
